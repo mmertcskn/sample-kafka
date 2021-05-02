4db17ba43d1d66ff2bcc1914c9279996c72db296
mrtcskn <mmert@sabanciuniv.edu>
2021-05-02 06:40:11 +0300

    kafka project structured

---
+++ src/main/java/com/mmertcskn/samplekafkaproject/SampleKafkaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mmertcskn.samplekafkaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleKafkaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleKafkaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmertcskn/samplekafkaproject/SampleKafkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmertcskn.samplekafkaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleKafkaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
